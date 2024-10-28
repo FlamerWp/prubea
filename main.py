807bd4d945d1f3c982875762fde647f24a71f262
FlamerWp <javierzsantiago@gmail.com>
2024-10-28 09:30:15 -0500

    Se realizan la validacion de errores

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from models.modulos import *
+try:
+    print ("Calculadora ")
+    print ("1. Suma")
+    print ("2. Resta")
+    print ("3. Multiplicacion")
+    print ("4. Division")
+    opc = input("Ingrese la opcion de lo que quiere realizar: ")
 
-print ("Calculadora ")
-print ("1. Suma")
-print ("2. Resta")
-print ("3. Multiplicacion")
-print ("4. Division")
-opc = input("Ingrese la opcion de lo que quiere realizar: ")
-
-n1 = int(input ("Ingrese el primer valor para la operacion: "))
-n2 = int(input ("Ingrese el segundo valor para la operacion: "))
-match opc:
-    case "1":
-        suma (n1, n2)
-    case "2":
-        resta (n1, n2)
-    case "3":
-        multiplicacion (n1, n2)
-    case "4":
-        division (n1,n2)
-    case _:
-        print ("Opcion no valida")        
+    n1 = int(input ("Ingrese el primer valor para la operacion: "))
+    n2 = int(input ("Ingrese el segundo valor para la operacion: "))
+    match opc:
+        case "1":
+            suma (n1,n2)
+        case "2":
+            resta (n1,n2)
+        case "3":
+            multiplicacion (n1,n2)
+        case "4":
+            division (n1,n2)
+        case _:
+            print ("Opcion no valida")        
+except TypeError: 
+    print ("No es valida la opcion")
         
