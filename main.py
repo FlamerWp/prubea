0d866933fc49b38e7938bcd36f9e4d9226a181f0
FlamerWp <javierzsantiago@gmail.com>
2024-10-28 09:07:08 -0500

    Se realiza el menu del aplicativo

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+print ("Calculadora ")
+print ("1. Suma")
+print ("2. Resta")
+print ("3. Multiplicacion")
+print ("4. Division")
+opc = input("Ingrese la opcion de lo que quiere realizar: ")
+
+match opc:
+    case "1":
+        print ("Suma")
+    case "2":
+        print ("Resta")
+    case "3":
+        print ("Multiplicacion")
+    case "4":
+        print ("Division")
+    case _:
+        print ("Opcion no valida")        
+        
+        
+        
